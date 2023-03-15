b8175e61dcdfad45e36562977e813a72ee37a856
ND Morsalin <morsalin056@gmail.com>
2023-03-14 20:16:06 -0700

    sort by range and sort by price is done

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -24,20 +24,20 @@ generateTag(tagLines, 3000);
 document
   .getElementById("random-room-btn")
   .addEventListener("click", async function () {
-    console.log("hello world");
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
-    
-    
+    const { name, summary, property_type, images, review_scores } =
+      result[randomIndex];
+
+    console.log({ images, name, summary, property_type, review_scores });
 
     const reviews = document.createElement("ol");
     if (review_scores.scores) {
-      reviews.setAttribute('class',"list-group")
+      reviews.setAttribute("class", "list-group");
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
@@ -48,30 +48,24 @@ document
     } else {
       reviews.innerHTML = "No reviews found";
     }
-
     
     modalBody.innerHTML = `
     <div class="col">
     <div class="card h-100">
-      <img src=${images.picture_url} class="card-img-top " alt="..." style=" height: 300px;
-      object-fit: fill;">
-      <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">${property_type}</p>
-        <p class="card-text">${summary}</p>
-        <div id='review-score'>
-      Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
-    </div>
+        <img src=${
+          images.picture_url
+        } class="card-img-top" alt="..." style=" height: 300px; object-fit: fill;">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${property_type}</p>
+          <p class="card-text">${summary}</p>
+          <div id='review-score'>
+            Review Scores :
+            <div id="review"></div>
+          </div>
+        </div>
+        <button class="btn btn-info btn-lg"  role="button">Show details</button>
       </div>
-      <button class="btn btn-info btn-lg"  role="button"
-                   >Show details</button>
-    </div>
-   </div>
-    `;
-    
+    </div>`;
+    document.getElementById('review').appendChild(reviews)
   });
-
-
-
- 
